160270d2e4223591c24a4f199f45e04653732f18
superslimguru <54024246+superslimguru@users.noreply.github.com>
2020-04-09 19:57:59 +0530

    Add files via upload

---
+++ research.c
@@ -0,0 +1,43 @@
+/*Researchers designed one system that classified interactive and noninteractive processes automatically by looking at the amount of terminal I/O.
+ If a process did not input or output to the terminal in a 1-second interval, the process was classified as noninteractive and was moved to a 
+lower-priority queue. In response to this policy, one programmer modified his programs to write an arbitrary character to the terminal at regular 
+intervals of less than 1 second. 
+The system gave his programs a high priority, even though the terminal output was completely meaningless.*/
+#include<stdio.h>
+int main()
+{
+	int i, type[20],n;
+	int resptime[20];
+	printf("Number of process: ");
+	scanf("%d",&n);
+	printf("Enter the data\n");
+	for(i=0;i<n;i++)
+	{
+		printf("Response time of P%d (in milliseconds): ",i);
+		scanf("%d",&resptime[i]);
+		if(resptime[i]<1000)
+		{
+			type[i]=1;
+		}
+		else
+		{
+			type[i]=0;
+		}
+	}
+	printf("Process Number\tResponse Time\tType\t\tPriority");
+	for(i=0;i<n;i++)
+	{
+		printf("\nP%d\t\t%dms\t\t",i,resptime[i]);
+		if(type[i]==1)
+		{
+			printf("Interactive\tHigh");
+		}
+		else
+		{
+			printf("Non-Interactive\tLow");
+		}
+	}
+}
+
+
+
