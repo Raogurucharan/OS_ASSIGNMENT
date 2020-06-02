7cccac3d4f43995eb8354c3627f91eedd1dabd5b
Gurucharan rao <54024246+superslimguru@users.noreply.github.com>
2020-06-02 21:40:30 +0530

    Update research.c

--- research.c
+++ research.c
@@ -2,7 +2,8 @@
  If a process did not input or output to the terminal in a 1-second interval, the process was classified as noninteractive and was moved to a 
 lower-priority queue. In response to this policy, one programmer modified his programs to write an arbitrary character to the terminal at regular 
 intervals of less than 1 second. 
-The system gave his programs a high priority, even though the terminal output was completely meaningless.*/
+The system gave his programs a high priority, even though the terminal output was completely meaningless.
+*/
 #include<stdio.h>
 int main()
 {
